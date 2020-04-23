572630a1513b4be5dfdefdcdf62d9c0b9ebee5f3
ronandrevon <ronandrevon@gmail.com>
2020-04-23 21:37:48 +0100

    added setup.py for scattering library

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+#with open("latex/README.md", "r") as fh:
+#    long_description = fh.read()
+
+setuptools.setup(
+    name="scattering",
+    version="0.0.1",
+    author="Tarik Ronan Drevon",
+    author_email="ronandrevon@gmail.com",
+    description="scattering utilities",
+    long_description='', #long_description,
+    long_description_content_type="",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License ",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['matplotlib','numpy','scipy','colorama','pandas'],
+)

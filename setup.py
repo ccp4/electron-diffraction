f5eaa55c4e28c787ffc3d3bf54abc401a0f6d25c
ronandrevon <ronandrevon@gmail.com>
2020-06-26 13:13:34 +0100

    changes to gitignore

--- setup.py
+++ setup.py
@@ -19,5 +19,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    install_requires=['matplotlib','numpy','scipy','colorama','pandas'],
+    #install_requires=['matplotlib','numpy','scipy','colorama','pandas'],
 )

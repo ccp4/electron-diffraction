96f3bc852292ea3cd11734555202cb75b0180b90
tdrevon <ronandrevon@gmail.com>
2022-02-07 13:28:49 +0000

    bug fix

--- tests/blochwave/test_bloch_base.py
+++ tests/blochwave/test_bloch_base.py
@@ -47,33 +47,6 @@ def test_show_Idyn_vs_Ikin():
     return b0.show_Idyn_vs_Ikin(iZs=[10,100,500,999],opt='')#slice(249,None,250))
 
 
-# @pytest.mark.slow
-def test_bloch_convergence():
-    # b0 = bloch.Bloch('diamond',path=out,keV=200,u=[2,3,1],Nmax=4,Smax=0.05,
-    #     opts='svt',thick=100)
-
-    # Nmaxs = np.arange(4,4*n+1,4)
-    refl=b0.get_beam(cond={'tol':1e-10,'opt':''},index=False)#;print(idx)
-    # refl = [[0,0,0],[2,2,0],[4,0,0],[4,4,0]]
-    # convergence_test(Smax=(0.05,0.04),Nmax=(6))
-    # Smax,Nmax=(0.05,0.1,0.2,0.5),(6)
-    Smax,Nmax=(0.1),(6,8,10,12)
-    b0.convergence_test(Smax=Smax,Nmax=Nmax)#,hkl=refl)
-    return b0.show_convergence(hkl=refl,xlab='Nmax')
-    # refl = [str(tuple(h))  for h in refl]
-    # nbs = len(refl)
-    # I,b = np.zeros((Nmaxs.size,nbs)),[]
-    # for i,Nmax in enumerate(Nmaxs):
-    #     b0 = bloch.Bloch('diamond',Smax=0.035,u=[0,0,1],Nmax=Nmax,thicks=(0,1000,10),thick=1000,opts='',path='dat/',name='diamond001_%d' %Nmax)
-    #     I[i,:]  = b0.df_G.loc[refl,'I'].values
-    #     b += [b0]
-    #
-    # cs = dsp.getCs('jet',nbs)
-    # plts = [[Nmaxs,I[:,iB],[cs[iB],'-o'],'%s' %h] for iB,h in enumerate(refl)]
-    # dsp.stddisp(plts,labs=['Nmax','I'],lw=2,opt='')
-    # return b
-
-
 
 @pytest.mark.lvl2
 def test_set_beam():
@@ -104,6 +77,33 @@ def test_show_Fhkl():
     b0.show_Fhkl(xylims=6,h3D=1,ms=30,cmap='Greens',opt='')
     b0.show_Fhkl(s=np.s_[:,:,3],xylims=6,ms=30,cmap='Greens',opt='')
     b0.show_Fhkl(s='h=0',opts='l',xylims=7,ms=50,cmap='Greens',pOpt='im',caxis=[0,1.5],opt='')
+
+# @pytest.mark.slow
+def test_bloch_convergence():
+    # b0 = bloch.Bloch('diamond',path=out,keV=200,u=[2,3,1],Nmax=4,Smax=0.05,
+    #     opts='svt',thick=100)
+
+    # Nmaxs = np.arange(4,4*n+1,4)
+    refl=b0.get_beam(cond={'tol':1e-10,'opt':''},index=False)#;print(idx)
+    # refl = [[0,0,0],[2,2,0],[4,0,0],[4,4,0]]
+    # convergence_test(Smax=(0.05,0.04),Nmax=(6))
+    # Smax,Nmax=(0.05,0.1,0.2,0.5),(6)
+    Smax,Nmax=(0.1),(6,8,10,12)
+    b0.convergence_test(Smax=Smax,Nmax=Nmax)#,hkl=refl)
+    return b0.show_convergence(hkl=refl,xlab='Nmax')
+    # refl = [str(tuple(h))  for h in refl]
+    # nbs = len(refl)
+    # I,b = np.zeros((Nmaxs.size,nbs)),[]
+    # for i,Nmax in enumerate(Nmaxs):
+    #     b0 = bloch.Bloch('diamond',Smax=0.035,u=[0,0,1],Nmax=Nmax,thicks=(0,1000,10),thick=1000,opts='',path='dat/',name='diamond001_%d' %Nmax)
+    #     I[i,:]  = b0.df_G.loc[refl,'I'].values
+    #     b += [b0]
+    #
+    # cs = dsp.getCs('jet',nbs)
+    # plts = [[Nmaxs,I[:,iB],[cs[iB],'-o'],'%s' %h] for iB,h in enumerate(refl)]
+    # dsp.stddisp(plts,labs=['Nmax','I'],lw=2,opt='')
+    # return b
+
 
 # test_bloch_convergence()
 

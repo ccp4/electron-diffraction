86ece6522b8f34a8c7d96b8d8e0dbf2e2a98cfd4
tdrevon <ronandrevon@gmail.com>
2024-02-18 01:08:50 +0000

    initial automatic versioning

--- blochwave/__init__.py
+++ blochwave/__init__.py
@@ -1,1 +1,3 @@
-version='1.0'
+import utils,os
+from . import _version
+__version__=utils._check_version(os.path.join(os.path.dirname(__file__)),_version.version)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ccp4ED",
-    version="1.1.0",
+    version="1.3",
     author="Tarik Ronan Drevon",
     author_email="tarik.drevon@stfc.ac.uk",
     description="Electron diffraction utilities",
